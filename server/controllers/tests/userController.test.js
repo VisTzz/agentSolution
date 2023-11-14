93393aea95ae229bf4e92a0b33c84292d4edc2f3
Vistzz <nightclose@mail.ru>
2023-11-15 00:13:13 +1000

    server: add jest and mock test

---
+++ server/controllers/tests/userController.test.js
@@ -0,0 +1,9 @@
+const userController = require('../userController')
+
+describe("user controller", () => {
+    describe("login", () => {
+        it("success", async () => {
+            expect('true').toBe('true');
+        })        
+    })
+})
--- server/database.sql
+++ server/database.sql
@@ -1,17 +1,31 @@
-create TABLE person(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    surname VARCHAR(255)
+CREATE TABLE IF NOT EXISTS public.contragents
+(
+    id integer NOT NULL DEFAULT nextval('contragents_id_seq'::regclass),
+    body character varying(255) COLLATE pg_catalog."default",
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT contragents_pkey PRIMARY KEY (id)
 )
 
-create TABLE post(
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    content VARCHAR(255),
-    user_id VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES person (id)
+CREATE TABLE IF NOT EXISTS public.parties
+(
+    id integer NOT NULL DEFAULT nextval('parties_id_seq'::regclass),
+    body character varying(255) COLLATE pg_catalog."default",
+    type character varying(255) COLLATE pg_catalog."default",
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT parties_pkey PRIMARY KEY (id)
 )
 
-INSERT INTO public.users(
-	id, email, password, role, "createdAt", "updatedAt")
-	VALUES (0, 'admin', 'admin', 'admin', NOW(), NOW());
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    email character varying(255) COLLATE pg_catalog."default",
+    password character varying(255) COLLATE pg_catalog."default",
+    role character varying(255) COLLATE pg_catalog."default" DEFAULT 'USER'::character varying,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "partyId" integer,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_email_key UNIQUE (email)
+)s
