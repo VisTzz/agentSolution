4338dd467fc7a6d13673b724289a1cc80c215c95
German <rybakov.german@sogaz.ru>
2023-11-28 00:40:36 +1000

    server: add docker-compose

--- server/database/databaseInit.sql
+++ server/database/databaseInit.sql
@@ -1,25 +1,23 @@
 CREATE TABLE IF NOT EXISTS public.contragents
 (
-    id integer NOT NULL DEFAULT nextval('contragents_id_seq'::regclass),
+    id integer PRIMARY KEY,
     body character varying(255) COLLATE pg_catalog."default",
     "createdAt" timestamp with time zone NOT NULL,
-    "updatedAt" timestamp with time zone NOT NULL,
-    CONSTRAINT contragents_pkey PRIMARY KEY (id)
-)
+    "updatedAt" timestamp with time zone NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public.parties
 (
-    id integer NOT NULL DEFAULT nextval('parties_id_seq'::regclass),
+    id integer PRIMARY KEY,
     body character varying(255) COLLATE pg_catalog."default",
     type character varying(255) COLLATE pg_catalog."default",
     "createdAt" timestamp with time zone NOT NULL,
-    "updatedAt" timestamp with time zone NOT NULL,
-    CONSTRAINT parties_pkey PRIMARY KEY (id)
-)
+    "updatedAt" timestamp with time zone NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS public.users
 (
-    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    id integer NOT NULL,
     email character varying(255) COLLATE pg_catalog."default",
     password character varying(255) COLLATE pg_catalog."default",
     role character varying(255) COLLATE pg_catalog."default" DEFAULT 'USER'::character varying,
@@ -28,4 +26,4 @@ CREATE TABLE IF NOT EXISTS public.users
     "partyId" integer,
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT users_email_key UNIQUE (email)
-)s
+)
